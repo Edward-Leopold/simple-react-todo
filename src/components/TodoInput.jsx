6ecffb33850aeb840de10c72971887b2af386e8a
Daniil <verybusyman17@gmail.com>
2022-09-28 20:32:31 +0300

    Initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,21 @@
+const TodoInput = ({ todo, setTodo, addTodo }) => {
+    return (
+        <div className="input-wrapper">
+            <input
+                type="text"
+                name="todo"
+                placeholder="Create a new todo"
+                value={todo}
+                onChange={(e) => {
+                    setTodo(e.target.value);
+                }}
+            />
+            <button
+                onClick={addTodo}
+                className="add-button"
+            >Add</button>
+        </div>
+    );
+};
+
+export default TodoInput;
